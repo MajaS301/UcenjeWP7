5ed21ddbcf85f3507207cc6bc990ad582c487124
MajaS301 <maja5steler@gmail.com>
2024-11-27 20:18:04 +0100

    promjena

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -6,6 +6,11 @@ create database edunovawp7;
 use edunovawp7;
 go
 
-create table smjer(
-naziv varchar (50)
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+trajanje int,
+cijena decimal(18,2),
+vaucer bit,
+izvodiseod datetime
 );
