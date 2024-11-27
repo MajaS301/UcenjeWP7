730d73c5f78ec771fbb5911408bbde9735d49a27
MajaS301 <maja5steler@gmail.com>
2024-11-27 21:00:24 +0100

    kreiranje baze practice

---
+++ vjezbanje.sql
@@ -0,0 +1,23 @@
+﻿use master 
+go
+drop database if exists practice;
+go
+create database practice;
+go
+use practice;
+
+create table ispitnirokovi (
+sifra int, 
+predmet varchar(50),
+vrstaIspita varchar(50),
+datum datetime,
+pristupio varchar(50)
+);
+
+
+create table pristupnici(
+ispitnirok datetime,
+student varchar(50),
+brojbodova int,
+ocjena varchar(50)
+);
