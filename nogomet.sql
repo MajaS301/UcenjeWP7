394573f77abd2d418ce781b531886b0a978adf80
MajaS301 <maja5steler@gmail.com>
2024-12-03 22:11:47 +0100

    kreiranje tablice utakmica

--- nogomet.sql
+++ nogomet.sql
@@ -8,11 +8,22 @@ use nogomet;
 go
 
 create table klub(
-sifra int not null primary key identity (2,2),
-naziv varchar (50) not null,
+sifra int not null identity (2,2),
+naziv varchar (50) not null primary key,
 osnovan int,
 stadion varchar (50),
 predsjednik varchar (50) not null,
 drzava varchar (50) not null,
 liga varchar (20)
+);
+
+
+create table utakmica(
+sifra int not null primary key identity(2,2),
+datum date,
+vrijeme time,
+lokacija varchar (50),
+stadion varchar (50) not null,
+domaci_klub varchar(50) not null references klub(naziv),
+gostujuci_klub varchar (50) not null references klub(naziv)
 );
