e39535f812a79bb6ef44e24b25d69e97bd0e338b
MajaS301 <maja5steler@gmail.com>
2024-12-03 21:58:58 +0100

    kreiranje tablice klub

---
+++ nogomet.sql
@@ -0,0 +1,18 @@
+﻿use master
+go
+drop database if exists nogomet;
+go
+create database nogomet;
+go
+use nogomet;
+go
+
+create table klub(
+sifra int not null primary key identity (2,2),
+naziv varchar (50) not null,
+osnovan int,
+stadion varchar (50),
+predsjednik varchar (50) not null,
+drzava varchar (50) not null,
+liga varchar (20)
+);
