908a2906f1f132c191fb2dbc69826f07ef35f04d
MajaS301 <maja5steler@gmail.com>
2025-01-09 20:44:29 +0100

    select na jednoj tablici

--- selectNaredba.sql
+++ selectNaredba.sql
@@ -24,3 +24,14 @@ select * from polaznici where sifra between 10 and 15;
 
 select * from polaznici where sifra=10 or sifra=15 or sifra=1;
 select * from polaznici where sifra in (10,15,1);
+
+select * from polaznici where ime like '%a';
+
+select * from smjerovi;
+
+update smjerovi 
+set izvodiseod='2024-10-02 18:30'
+where sifra=4;
+
+select * from smjerovi
+where izvodiseod between '2021-01-01' and '2024-06-30'
