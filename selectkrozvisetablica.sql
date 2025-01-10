611c39b5b01f7414ae45611e8146cd29db57467e
MajaS301 <maja5steler@gmail.com>
2025-01-10 20:51:20 +0100

    prvi dio

---
+++ selectkrozvisetablica.sql
@@ -0,0 +1,24 @@
+﻿select a.naziv, b.naziv as grupa, b.predavac
+from smjerovi a inner join grupe b
+on a.sifra=b.smjer
+
+
+select a.naziv, b.naziv as grupa, b.predavac
+from smjerovi a left join grupe b
+on a.sifra=b.smjer
+
+
+select b.naziv as grupa,
+a.naziv as smjer,
+d.ime, d.prezime
+from smjerovi a 
+left join grupe b on a.sifra=b.smjer
+left join clanovi c on b.sifra=c.grupa
+left join polaznici d on d.sifra=c.polaznik;
+
+
+
+update grupe set predavac='Karla Let' where sifra=4
+
+
+select * from smjerovi;
