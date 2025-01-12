deae1dc0bb70d8492f6f5b6de6a7adf52f966983
MajaS301 <maja5steler@gmail.com>
2025-01-12 11:00:45 +0100

    select u knjiznici

--- selectkrozvisetablica.sql
+++ selectkrozvisetablica.sql
@@ -8,13 +8,13 @@ from smjerovi a left join grupe b
 on a.sifra=b.smjer
 
 
-select b.naziv as grupa,
+select b.naziv as grupa       ,
 a.naziv as smjer,
 d.ime, d.prezime
 from smjerovi a 
-left join grupe b on a.sifra=b.smjer
-left join clanovi c on b.sifra=c.grupa
-left join polaznici d on d.sifra=c.polaznik;
+inner join grupe b on a.sifra=b.smjer
+inner join clanovi c on b.sifra=c.grupa
+inner join polaznici d on d.sifra=c.polaznik;
 
 
 
@@ -22,3 +22,26 @@ update grupe set predavac='Karla Let' where sifra=4
 
 
 select * from smjerovi;
+
+
+use knjiznica;
+
+select concat (a.ime, ' ', a.prezime) as autor,
+b.naslov, c.naziv as izdavac, 
+d.naziv as mjesto
+from autor a
+inner join katalog b on a.sifra = b.autor
+inner join izdavac c on c.sifra = b.izdavac
+inner join mjesto d on d.sifra = b.mjesto
+where a.ime like 'I%'
+order by a.prezime;
+
+
+-- alternativa
+select autor.ime, autor.prezime,
+katalog.naslov, izdavac.naziv as izdavac,
+mjesto.naziv as mjesto
+from autor
+inner join katalog on autor.sifra = katalog.autor
+inner join izdavac on izdavac.sifra = katalog.izdavac
+inner join mjesto on mjesto.sifra = katalog.mjesto;
